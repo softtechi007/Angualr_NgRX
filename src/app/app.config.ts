0589c734650e1dc277575e5428290ae338fd257d
softtechie007 <iqbalhamsa01@gmail.com>
2025-09-18 20:45:51 +0530

    18 - Added Redux dev tool for further development

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './counter/states/counter.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideStore({ counter: counterReducer }) // Add reducer to the store
+    provideStore({ counter: counterReducer }), // Add reducer to the store
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
 ]
 };
